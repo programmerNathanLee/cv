3b75b07c0d175b975dde5c9fb1bf9c96457c3d98
nathan gartner <nathangartner@uvic.ca>
2020-12-30 12:34:54 -0800

    Script file for date/time and index file update

---
+++ HTML Personal Site/js/scripts.js
@@ -0,0 +1,40 @@
+function renderTime(){
+
+// Date
+  var mydate = new Date();
+  var year = mydate.getYear();
+    if(year<1000){
+      year += 1900;
+    }
+  var day = mydate.getDay();
+  var month = mydate.getMonth();
+  var daym = mydate.getDate();
+  var dayarray = new Array("Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday");
+  var montharray = new Array("January","February","March","April","May","June","July","August","September","October","November","December");
+  // Date renderTime
+
+  // Time
+  var currentTime = new Date90:
+  var h = currentTime.getHours();
+  var m = currentTime.getMinutes();
+  var s = currentTime.getSeconds();
+
+    if(h > 12){
+      h = h - 12;
+    }
+
+    if (m < 10){
+      m = "0" + m;
+    }
+
+    if (s < 10){
+      s = "0" + s;
+    }
+
+    var myClock = document.getElementById("clockDisplay");
+    myClock.textContent = "" +dayarray[day]+ ", " +daym+ " " +montharray[month]+ " " +year+ " | " +h+ ":" +m+ ":" +s+;
+    myClock.innerText = "" +dayarray[day]+ ", " +daym+ " " +montharray[month]+ " " +year+ " | " +h+ ":" +m+ ":" +s+;
+
+    setTimeout("renderTime()", 1000");
+}
+renderTime();
